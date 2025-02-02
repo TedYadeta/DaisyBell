0155f980c21793903a776ce8a97e5e63bf9746da
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2025-02-02 12:55:24 -0800

    Add files via upload

---
+++ DaisyBell.ps1
@@ -0,0 +1,345 @@
+﻿# Daisy Bell in PowerShell
+#
+# Using just [System.Console]::Beep(x, 1000)
+# in which x is a tone which equals a note
+# lets play Daisy Bell!
+#
+# Special Thanks to Joy Shepard for helping me understand scales and tones
+#
+# Notes = Tones
+# -------------
+
+$C4 = 262
+$D4 = 293
+$E4 = 329
+$F4 = 349
+$G4 = 392
+$A4 = 440
+$B4 = 493
+$C5 = 523
+$D5 = 587
+$E5 = 659
+$F5 = 698
+$G5 = 783
+
+# Note Lengths
+# ------------
+$full = 1000
+$half = 500
+$quarter = 250
+$eighth = 125
+
+[System.Console]::Beep($D4, $quarter)
+[System.Console]::Beep($C5, $quarter)
+[System.Console]::Beep($D4, $quarter)
+
+# There is a
+
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# flow-er with
+
+[System.Console]::Beep($A4, $half)
+[System.Console]::Beep($F4, $quarter)
+
+# in my
+
+[System.Console]::Beep($D4, $half)
+
+# heart
+
+[System.Console]::Beep($A4, 750)
+[System.Console]::Beep($D4, 750)
+
+# Dai - sy
+
+
+[System.Console]::Beep($D5, 750) 
+[System.Console]::Beep($B4, $half)
+
+# Dai - sy!
+
+[System.Console]::Beep($D5, $quarter)
+[System.Console]::Beep($C5, $quarter)
+[System.Console]::Beep($D5, $quarter)
+
+# Plant-ed one
+
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# day by a
+
+[System.Console]::Beep($A4, $half)
+[System.Console]::Beep($F4, $quarter)
+
+# glanc - ing
+
+[System.Console]::Beep($D4, $half)
+
+# dart
+
+# #######################################
+
+[System.Console]::Beep($G4, $quarter)
+[System.Console]::Beep($D5, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# Plant-ed by
+
+
+[System.Console]::Beep($F4, $quarter)
+[System.Console]::Beep($D4, $quarter)
+[System.Console]::Beep($A4, $quarter)
+
+# Dai - sy
+
+[System.Console]::Beep($G4, 750)
+
+# Bell!...
+
+
+[System.Console]::Beep($G4, $quarter)
+
+
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($C4, $quarter)
+[System.Console]::Beep($B4, $quarter)
+
+# Wheth-er she
+
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+[System.Console]::Beep($F4, $quarter)
+
+# loves me or
+
+[System.Console]::Beep($B4, $half)
+[System.Console]::Beep($G4, $quarter)
+
+# loves me
+
+
+######################
+# Second Bar, 2nd Page
+######################
+
+
+[System.Console]::Beep($E4, $half)
+
+# not
+
+[System.Console]::Beep($F4, $quarter)
+[System.Console]::Beep($D4, $quarter)
+[System.Console]::Beep($B4, $quarter)
+
+# Sometimes it's
+
+
+[System.Console]::Beep($A4, $half)
+[System.Console]::Beep($A4, $quarter)
+
+# hard to
+
+
+
+[System.Console]::Beep($B4, 750)
+
+# tell
+
+
+[System.Console]::Beep($B4, $quarter)
+
+
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($C5, $quarter)
+[System.Console]::Beep($B4, $quarter)
+
+# Yet I am
+
+
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+[System.Console]::Beep($F4, $quarter)
+
+# long-ing to
+
+
+[System.Console]::Beep($B4, $half)
+[System.Console]::Beep($G4, $quarter)
+
+# share the
+
+
+[System.Console]::Beep($E4, $half)
+[System.Console]::Beep($B4, $quarter)
+
+# lot Of
+
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($E5, $quarter)
+[System.Console]::Beep($C5, $quarter)
+
+# beau-ti-ful
+
+[System.Console]::Beep($B4, $half)
+[System.Console]::Beep($A4, $quarter)
+
+# Dai - sy 
+
+[System.Console]::Beep($D5, 750)
+[System.Console]::Beep($D5, $half)
+
+# Bell!.....
+
+
+# Third Page, First Bar
+# ###########################
+
+[System.Console]::Beep($D5, 750)
+[System.Console]::Beep($B4, 750)
+
+# Dai-sy
+
+[System.Console]::Beep($G4, 750)
+[System.Console]::Beep($D4, $half)
+
+# Dai-sy
+
+[System.Console]::Beep($E4, $quarter)
+[System.Console]::Beep($F4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# Give me your
+
+[System.Console]::Beep($E4, $half)
+[System.Console]::Beep($G4, $quarter)
+
+# an -swer,
+
+[System.Console]::Beep($D4, 750)
+[System.Console]::Beep($D4, $half)
+
+# do!....
+
+
+#
+# Third Page, Second Bar
+#
+
+[System.Console]::Beep($A4, 750)
+
+# I'm
+
+[System.Console]::Beep($D5, 750)
+
+# half
+
+[System.Console]::Beep($B4, 750)
+
+# cra -
+
+[System.Console]::Beep($G4, $half)
+
+# -zy
+
+[System.Console]::Beep($E4, $quarter)
+[System.Console]::Beep($F4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# All for the
+
+[System.Console]::Beep($A4, $half)
+[System.Console]::Beep($B4, $quarter)
+
+# love of
+
+[System.Console]::Beep($A4, 750)
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($B4, $quarter)
+
+# you!....It
+
+[System.Console]::Beep($C5, $quarter)
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($A4, $quarter)
+
+# won't be a 
+
+[System.Console]::Beep($D5, $half)
+[System.Console]::Beep($B4, $quarter)
+
+# styl - ish
+
+[System.Console]::Beep($A4, $quarter)
+[System.Console]::Beep($G4, $half)
+
+# mar - riage 
+
+[System.Console]::Beep($G4, $half)
+[System.Console]::Beep($A4, $quarter)
+
+# I
+
+[System.Console]::Beep($B4, $half)
+[System.Console]::Beep($G4, $quarter)
+
+# can't af-
+
+[System.Console]::Beep($E4, $half)
+[System.Console]::Beep($G4, $quarter)
+
+# - ford a
+
+[System.Console]::Beep($E4, $quarter)
+[System.Console]::Beep($D4, $half)
+
+# car-riage,....
+
+[System.Console]::Beep($D4, $quarter)
+[System.Console]::Beep($D4, $quarter)
+
+# But
+
+[System.Console]::Beep($G4, $half)
+[System.Console]::Beep($B4, $quarter)
+
+# you'll look
+
+[System.Console]::Beep($A4, $quarter)
+
+# sweet
+
+
+
+[System.Console]::Beep($G4, $half)
+[System.Console]::Beep($B4, $quarter)
+
+# On the
+
+[System.Console]::Beep($A4, $quarter)
+
+
+[System.Console]::Beep($B4, $eighth)
+[System.Console]::Beep($C5, $eighth)
+
+# seat of a
+
+[System.Console]::Beep($D5, $quarter)
+[System.Console]::Beep($B4, $quarter)
+[System.Console]::Beep($G4, $quarter)
+
+# bicycle
+
+[System.Console]::Beep($A4, $half)
+[System.Console]::Beep($D4, $quarter)
+
+# built for
+
+[System.Console]::Beep($G4, 750)
+[System.Console]::Beep($G4, $quarter)
+
+# two!....
